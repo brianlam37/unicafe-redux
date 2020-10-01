87cd826f18071163b19d035796fa216f81897816
brianlam37 <brianlam37@gmail.com>
2020-10-01 01:08:56 -0700

    finish unicode-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from 'react';
+import {createStore} from 'redux';
+import counterReducer from './reducers/counterReducer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let store = createStore(counterReducer);
+const Statistics = () => {
+	const state = store.getState();
+	let positive = 0;
+	let all = state.good + state.ok + state.bad;
+	let average = 0;
+	if(all === 0){
+		return(
+			<div>
+				<h1>statistics</h1>
+				<table>
+					<p>no feedback given yet</p>
+				</table>
+			</div>
+		);
+	}else{
+		positive = state.good/all * 100;
+		average = (state.good + state.bad*-1)/all;
+		return(
+			<div>
+				<h1>statistics</h1>
+				<table>
+					<Statistic text = 'good' value = {state.good}/>
+					<Statistic text ='ok' value = {state.ok}/>
+					<Statistic text ='bad' value = {state.bad}/>
+					<Statistic text ='all' value = {all}/>
+					<Statistic text ='average' value = {average}/>
+					<Statistic text ='positive' value = {positive+'%'}/>
+				</table>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+			</div>
+		);
+	}
+};
+const Statistic = ({text, value}) => {
+	return (
+		<tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
+	);
+};
+const App = () => {
+	// save clicks of each button to own state
+
+	const handleGood = () => {
+		store.dispatch({type: 'GOOD'});
+	};
+	const handleOk = () => {
+		store.dispatch({type: 'OK'});
+	};
+	const handleBad = () => {
+		store.dispatch({type: 'BAD'});
+	};
+	const handleZero = () => {
+		store.dispatch({type: 'ZERO'});
+	};
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<Button handleClick={() => handleGood()} text = 'good'/>
+			<Button handleClick={() => handleOk()} text = 'ok'/>
+			<Button handleClick={() => handleBad()} text = 'bad'/>
+			<Button handleClick={() => handleZero()} text = 'zero'/>
+			<Statistics good = {store.getState().good} ok = {store.getState().ok} bad={store.getState().bad}/>
+		</div>
+	);
+};
+
+const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>);
+
+const renderApp = () => {
+	ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
